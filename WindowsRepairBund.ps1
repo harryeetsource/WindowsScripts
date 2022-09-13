3d0a5bfb194c4b7b7c089aecef6029ab68fd453b
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-09-13 12:36:33 -0700

    Add files via upload

--- WindowsRepairBund.ps1
+++ WindowsRepairBund.ps1
@@ -6,6 +6,8 @@ $title2 = 'Would you like to install the new version of Powershell?'
 $title3 = 'Are you on windows 10?'
 $title4 = 'Would you like to create a restore point?'
 $title5 = 'Would you like to repair windows update services?'
+$url2 = "https://go.microsoft.com/fwlink/?LinkID=799445"
+$folder2 = "$env:appdata\WUA"
 function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
     $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
@@ -21,20 +23,17 @@ if ((Test-Admin) -eq $false)  {
 $decision = $Host.UI.PromptForChoice($title3, $question, $choices, 1)
 if ($decision -eq 0) {
     Write-Host 'confirmed'
-    $url2 = "https://go.microsoft.com/fwlink/?LinkID=799445"
-    $folder2 = "$env:appdata\WUA"
 
 if (Test-Path -Path $folder2) {Write-Host "WUA directory already exists, removing old version"
     Remove-Item -Path $folder2 -Recurse
     New-Item -Path "$env:appdata\" -Name "WUA" -ItemType "directory"
     Invoke-WebRequest $url2 -OutFile "$folder2\WUA.exe"
-    Start-Process "$folder2\WUA.exe"
-}
-else {
-    New-Item -Path "$env:appdata\" -Name "WUA" -ItemType "directory"
-    Invoke-WebRequest $url2 -OutFile "$folder2\WUA.exe"
     Start-Process "$folder2\WUA.exe"}
-}
+    else {
+        New-Item -Path "$env:appdata\" -Name "WUA" -ItemType "directory"
+        Invoke-WebRequest $url2 -OutFile "$folder2\WUA.exe"
+        Start-Process "$folder2\WUA.exe"}
+    }
 else {
     Write-Host 'cancelled'
 }
@@ -53,39 +52,47 @@ if ($decision -eq 0) {
     Write-Host 'confirmed'
 $url3 = "https://github.com/PowerShell/PowerShell/releases/download/v7.2.6/PowerShell-7.2.6-win-x64.msi"
 $folder3 = "$env:Temp\pwsh"
-if (Test-Path -Path $folder3) { Write-Host "pwsh directory already exists, skipping" }
-else {
-New-Item -Path "$env:temp\" -Name "pwsh" -ItemType "directory"
-Invoke-WebRequest  $url3 -OutFile "$folder3\pwsh.msi"
-Start-Process "$folder3\pwsh.msi" -ArgumentList "/quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=0 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1"}
+    if (Test-Path -Path $folder3) { Write-Host "pwsh directory already exists, skipping" }
+    else {
+    New-Item -Path "$env:temp\" -Name "pwsh" -ItemType "directory"
+    Invoke-WebRequest  $url3 -OutFile "$folder3\pwsh.msi"
+    Start-Process "$folder3\pwsh.msi" -ArgumentList "/quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=0 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1"}
 }
 else {
     Write-Host 'cancelled'
 }
+$registrykey = "HKLM:\System\CurrentControlSet\Services\VSS\VssAccessControl"
+$name = "$env:UserDomain\$env:UserName"
+$value = "1"
 $decision = $Host.UI.PromptForChoice($title4, $question, $choices, 1)
 if ($decision -eq 0) {
     Write-Host 'confirmed, attempting to create restore point'
-$a = Get-Date
-try {Enable-ComputerRestore -drive C:\
-Invoke-CimMethod -Namespace  root/DEFAULT -ClassName SystemRestore -MethodName CreateRestorePoint -Arguments @{
+    try {Enable-ComputerRestore -drive C:\
+	Invoke-CimMethod -Namespace  root/DEFAULT -ClassName SystemRestore -MethodName CreateRestorePoint -Arguments @{
     Description      = (Get-Date).ToString()
     RestorePointType = [uint32]0
     EventType        = [uint32]100
-}
-}
-catch { "unable to create restore point, starting gui"}
-Start-Process -Filepath "${env:Windir}\System32\rstrui.exe"
-Start-Process -Filepath "${env:Windir}\System32\SystemPropertiesProtection.exe"
-}
+}}
+
+    catch { "unable to create restore point, starting gui and adding manual registry key"
+	Start-Process -Filepath "${env:Windir}\System32\rstrui.exe"
+	Start-Process -Filepath "${env:Windir}\System32\SystemPropertiesProtection.exe"}
+    if (test-path -path $registrykey){
+	Write-host "user already has VSS service access" }
+    else { New-Item -path $registrypath -Force | Out-Null
+	New-ItemProperty -path $registrypath -name $name -value $value -propertype DWORD -force | Out-Null
+}}
 else {
 Write-Host 'cancelled'
 }
 $decision = $Host.UI.PromptForChoice($title5, $question, $choices, 1)
 if ($decision -eq 0) { 
-Invoke-Webrequest -uri https://raw.githubusercontent.com/harryeetsource/WindowsScripts/9ddb625d7bcf939175a027930d9195ad1565a628/WuReset2.0.bat -OutFile "${env:temp}\wu.bat" && Start-Process -Filepath  "${env:temp}\wu.bat"
-
+Invoke-Webrequest -uri https://raw.githubusercontent.com/harryeetsource/WindowsScripts/9ddb625d7bcf939175a027930d9195ad1565a628/WuReset2.0.bat -OutFile "${env:temp}\wu.bat" 
+Start-Process -Filepath  "${env:temp}\wu.bat"
 }
 else {
 Write-Host 'cancelled'
 }
+Write-Host 'Admin updates completed.'
+Pause
 
