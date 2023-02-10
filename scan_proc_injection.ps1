0cac5818606fa6aedbc9949e773aedb0bca9502d
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-02-09 16:12:10 -0800

    Update scan_proc_injection.ps1

--- scan_proc_injection.ps1
+++ scan_proc_injection.ps1
@@ -1,38 +1,34 @@
-# Define the process to scan
-$processName = "processName.exe"
+# Loop through all processes
+Get-Process | Foreach-Object {
+    $process = $_
+    $pid = $process.Id
+    $processName = $process.ProcessName
+    # Get the process memory regions
+    $regions = Get-Process -Id $pid | Select-Object -ExpandProperty VM | Where-Object { $_.State -eq "Commit" }
+    # Iterate over each memory region
+    foreach ($region in $regions) {
+        # Get the region start and end addresses
+        $start = $region.BaseAddress
+        $end = [IntPtr]::Add($start, $region.RegionSize)
 
-# Get the process by its name
-$process = Get-Process | Where-Object { $_.ProcessName -eq $processName }
+        # Read the memory region
+        $memory = [System.Runtime.InteropServices.Marshal]::Copy($start, [Byte[]]::new($region.RegionSize), 0, $region.RegionSize)
 
-# Get the process ID
-$pid = $process.Id
+        # Check if the memory region contains a PE header
+        $peHeader = [System.IntPtr]::Zero
+        try {
+            $peHeader = [System.Reflection.Assembly]::Load($memory).GetModules()[0].PEHeader
+        } catch { }
 
-# Get the process memory regions
-$regions = Get-Process -Id $pid | Select-Object -ExpandProperty VM | Where-Object { $_.State -eq "Commit" }
+        # If the memory region contains a PE header, dump the region to disk
+        if ($peHeader -ne [System.IntPtr]::Zero) {
+            # Define the output file name
+            $outputFile = "$processName-0x$($start.ToString("X")).dll"
 
-# Iterate over each memory region
-foreach ($region in $regions) {
-    # Get the region start and end addresses
-    $start = $region.BaseAddress
-    $end = [IntPtr]::Add($start, $region.RegionSize)
+            # Write the memory region to disk
+            [System.IO.File]::WriteAllBytes($outputFile, $memory)
 
-    # Read the memory region
-    $memory = [System.Runtime.InteropServices.Marshal]::Copy($start, [Byte[]]::new($region.RegionSize), 0, $region.RegionSize)
-
-    # Check if the memory region contains a PE header
-    $peHeader = [System.IntPtr]::Zero
-    try {
-        $peHeader = [System.Reflection.Assembly]::Load($memory).GetModules()[0].PEHeader
-    } catch { }
-
-    # If the memory region contains a PE header, dump the region to disk
-    if ($peHeader -ne [System.IntPtr]::Zero) {
-        # Define the output file name
-        $outputFile = "$processName-0x$($start.ToString("X")).dll"
-
-        # Write the memory region to disk
-        [System.IO.File]::WriteAllBytes($outputFile, $memory)
-
-        Write-Output "Dumped injected code or portable executable to $outputFile"
+            Write-Output "Dumped injected code or portable executable to $outputFile"
+        }
     }
 }
